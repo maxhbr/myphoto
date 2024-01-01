910d2198ef35f4c267d402d78cfc6fd26bb7400e
Maximilian Huber <gh@maxhbr.de>
2024-01-01 22:27:36 +0100

    it works
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- new.hs/app/MyPhoto/Actions/Enfuse.hs
+++ new.hs/app/MyPhoto/Actions/Enfuse.hs
@@ -65,52 +65,6 @@ enfuseDefaultOptions
   , optSaveMasks  = False
   }
 
--- options :: [OptDescr (Options -> Options)]
--- options =
---   [ Option ['v'] ["verbose"]
---       (NoArg (\ opts -> opts { optEnfuseVerbose = True }))
---       "chatty output on stderr"
---   , Option ['h'] ["help"]
---       (NoArg (\ opts -> opts { optHelp = True }))
---       "print help"
---   , Option ['c'] ["chunks"]
---       (ReqArg (\f opts -> opts { optChunks = Just (read f) })
---        "CHUNKS")
---       "split stacking in smaller chunks"
---   , Option [] ["autochunk"]
---       (NoArg (\ opts -> opts { optChunks = Just (-1) }))
---       "split stacking in smaller chunks, by sqrt of number of images"
---   , Option ['p'] ["projection"]
---       (ReqArg (\f opts -> opts { optProjection = case f of
---                                    "1" -> Proj1
---                                    "2" -> Proj2
---                                    "3" -> Proj3
---                                    _   -> Proj1
---                                }) "PROJECTION_NUMBER")
---       "choose kind of projection"
---   , Option ['o'] ["opts"]
---       (ReqArg (\f opts -> opts { optOpts = case f of
---                                    "1" -> Opts1
---                                    "2" -> Opts2
---                                    "3" -> Opts3
---                                    _   -> Opts1
---                                }) "OPTS_NUMBER")
---       "choose kind of opts"
---   , Option ['t'] ["threads"]
---       (OptArg (\case
---                   Just capabilities              -> (\opts -> opts { optConcurrent = Just (read capabilities) })
---                   Nothing | numCapabilities == 1 -> (\opts -> opts { optConcurrent = Nothing} )
---                   Nothing                        -> (\opts -> opts { optConcurrent = Just (min (numCapabilities - 1) 9)} ))
---         "CAPABILITIES")
---       "run in parallel"
---   , Option ['a'] ["all"]
---       (NoArg (\ opts -> opts { optAll = True }))
---       "do all opts / projection variants"
---   , Option ['s'] ["save-masks"]
---       (NoArg (\ opts -> opts { optSaveMasks = True }))
---       "save the masks"
---   ]
-
 optionsToFilenameAppendix :: EnfuseOptions -> String
 optionsToFilenameAppendix o = let
     projectionOptionsToFilenameAppendix :: EnfuseOptions -> String
@@ -124,35 +78,6 @@ optionsToFilenameAppendix o = let
     chunkOptionsToFilenameAppendix EnfuseOptions {optChunks = Nothing } = ""
     chunkOptionsToFilenameAppendix EnfuseOptions {optChunks = Just n }  = "c" ++ show n
   in "_" ++ projectionOptionsToFilenameAppendix o ++ optsOptionsToFilenameAppendix o ++ chunkOptionsToFilenameAppendix o
-
--- help :: String
--- help = let
---     header = "Usage: stack [OPTION...] files..."
---     projectionsDesc = unlines [ "    Projections:"
---                               , "      1: " ++ show (projectionToArgs Proj1)
---                               , "      2: " ++ show (projectionToArgs Proj2)
---                               , "      3: " ++ show (projectionToArgs Proj3)
---                               ]
---     optionsDesc = unlines [ "    Opts:"
---                           , "      1: " ++ show (optsToArgs Opts1)
---                           , "      2: " ++ show (optsToArgs Opts2)
---                           , "      3: " ++ show (optsToArgs Opts3)
---                           ]
---     chunkDesc = unlines [ "    Chunks:"
---                         , "      n: if n > 1: split into chunks of size n"
---                         , "         else: calculate chunk size by sqrt of number of images"
---                         , "      if not present: do not split into chunks"
---                         ]
---   in unlines [ usageInfo header options
---              , projectionsDesc
---              , optionsDesc
---              , chunkDesc
---              ]
-
--- getMyOpts :: [String] -> IO (Options, [String])
--- getMyOpts argv = case getOpt Permute options argv of
---                    (o,n,[]  ) -> return (foldl (flip id) defaultOptions o, n)
---                    (_,_,errs) -> ioError (userError (concat errs ++ help))
 
 getEnfuseArgs :: EnfuseOptions -> [String]
 getEnfuseArgs opts = let
@@ -227,18 +152,18 @@ getStackedFilename opts img = let
         Nothing -> bn ++ "_STACKED" ++ optionsToFilenameAppendix opts <.> ext
         Just outputBN -> outputBN ++ "_STACKED" ++ optionsToFilenameAppendix opts <.> ext
 
-getChunkFilename :: FilePath -> Int -> Int -> FilePath
-getChunkFilename img indexOfChunk numberOfChunks = let
+getChunkFilename :: FilePath -> FilePath -> Int -> Int -> FilePath
+getChunkFilename workdir img indexOfChunk numberOfChunks = let
     (bn,ext) = splitExtensions img
-  in bn ++ "_CHUNK" ++ show indexOfChunk ++ "of" ++ show numberOfChunks <.> ext
+  in workdir </> (takeBaseName bn) ++ "_CHUNK" ++ show indexOfChunk ++ "of" ++ show numberOfChunks <.> ext
 
 enfuseStackImgs:: EnfuseOptions -> [FilePath] -> IO (Either String [FilePath])
 enfuseStackImgs opts' = let
 
-    stackImpl'' :: MS.MSem Int -> EnfuseOptions -> (FilePath, Bool, [String]) -> [FilePath] -> IO (Either String [FilePath])
-    stackImpl'' sem opts (outFile, saveMasks, enfuseArgs) imgs = case calculateNextChunkSize opts imgs of
+    stackImpl'' :: MS.MSem Int -> EnfuseOptions -> (FilePath, FilePath Bool, [String]) -> [FilePath] -> IO (Either String [FilePath])
+    stackImpl'' sem opts args@(outFile, workdir, saveMasks, enfuseArgs) imgs = case calculateNextChunkSize opts imgs of
       Nothing -> do
-        (MS.with sem . runEnfuse 2 (outFile, saveMasks, enfuseArgs)) imgs
+        (MS.with sem . runEnfuse 2 args) imgs
       Just maxChunkSize -> let
           chunks = chunksOf maxChunkSize imgs
           numberOfChunks = length chunks
@@ -247,12 +172,12 @@ enfuseStackImgs opts' = let
 
           chunkImgs <- mapConcurrently
                      (\(i,chunk) -> do
-                         let chunkOutputFilename = getChunkFilename outFile i numberOfChunks
-                         stackImpl'' sem opts (chunkOutputFilename, saveMasks, enfuseArgs) chunk)
+                         let chunkOutputFilename = getChunkFilename workdir outFile i numberOfChunks
+                         stackImpl'' sem opts (chunkOutputFilename, workdir, saveMasks, enfuseArgs) chunk)
                      (zip [1..] chunks)
 
           case foldl foldResults (Right []) chunkImgs of
-            Right chunkImgs' -> stackImpl'' sem opts (outFile, saveMasks, enfuseArgs) chunkImgs'
+            Right chunkImgs' -> stackImpl'' sem opts args chunkImgs'
             err              -> return err
 
     stackImpl' :: MS.MSem Int -> EnfuseOptions -> [FilePath] -> IO (Either String [FilePath])
@@ -261,7 +186,8 @@ enfuseStackImgs opts' = let
       in do
         let outFile = getStackedFilename opts (head imgs)
         let workdir = outFile -<.> "workdir"
-        stackImpl'' sem opts (outFile, optSaveMasks opts, enfuseArgs) imgs
+        createDirectoryIfMissing True workdir
+        stackImpl'' sem opts (outFile, workdir, optSaveMasks opts, enfuseArgs) imgs
 
   in \imgs -> do
     -- apply autochunk
