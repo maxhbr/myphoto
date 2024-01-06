7aaab62da7a74fb7613aa0a47e552e6f2f03be9a
Maximilian Huber <gh@maxhbr.de>
2024-01-06 21:23:22 +0100

    rewrite!
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- app/MyPhoto/Monad.hs
+++ app/MyPhoto/Monad.hs
@@ -16,6 +16,7 @@ import MyPhoto.Video
 import System.Console.GetOpt
 import System.Environment (getArgs, getProgName, withArgs)
 import qualified System.IO as IO
+import qualified GHC.IO.Handle as IO
 
 
 startOptions :: Options
@@ -39,7 +40,17 @@ data MyPhotoState
         myPhotoStateOuts :: Imgs,
         myPhotoStateWd :: Maybe FilePath
       }
-      deriving (Show)
+instance Show MyPhotoState where
+  show s =
+    "MyPhotoState {\n  myPhotoStateOpts = "
+      ++ show (myPhotoStateOpts s)
+      ++ ",\n  myPhotoStateImgs = "
+      ++ show (length (myPhotoStateImgs s))
+      ++ ",\n  myPhotoStateOuts = "
+      ++ show (myPhotoStateOuts s)
+      ++ ",\n  myPhotoStateWd = "
+      ++ show (myPhotoStateWd s)
+      ++ "\n}"
 startMyPhotoState :: Imgs -> MyPhotoState
 startMyPhotoState imgs =
   MyPhotoState
@@ -100,3 +111,24 @@ withOutsIO f = do
   addOuts outs'
 setWd :: FilePath -> MyPhotoM ()
 setWd wd = MTL.modify (\s -> s {myPhotoStateWd = Just wd})
+
+
+redirectLogToLogFile :: MyPhotoM a -> MyPhotoM a
+redirectLogToLogFile action = do
+  wd <- MTL.gets myPhotoStateWd
+  case wd of
+    Nothing -> action
+    Just wd' -> do
+      let logFile = wd' ++ "/myphoto.log"
+      logInfo $ "Redirecting log to " ++ logFile
+      state <- MTL.get
+      (ret, endState) <- MTL.liftIO $ IO.withFile logFile IO.AppendMode $ \logFile -> do
+        -- redirect stdout to log file without losing the actual output
+        IO.hDuplicateTo logFile IO.stdout
+        IO.hSetBuffering IO.stdout IO.LineBuffering
+        IO.hDuplicateTo logFile IO.stderr
+        IO.hSetBuffering IO.stderr IO.LineBuffering
+
+        MTL.runStateT action state
+      MTL.put endState
+      return ret
--- app/MyPhoto/Stack.hs
+++ app/MyPhoto/Stack.hs
@@ -226,210 +226,6 @@ getOutputBN = do
   imgs <- getImgs
   return (computeStackOutputBN imgs)
 
-#if 0
--- getOptionsAndInitialize :: [String] -> MTL.StateT (Options, [FilePath]) IO ()
--- getOptionsAndInitialize =
---   let getActionsFromArgs :: [String] -> MTL.StateT (Options, [FilePath]) IO [Options -> IO Options]
---       getActionsFromArgs args = do
---         (opts, imgs) <- MTL.get
---         let (actions, imgs', errors) = getOpt RequireOrder options args
---         MTL.put (opts, imgs ++ imgs')
-
---         unless (null errors) $ MTL.liftIO $ do
---           mapM_ (IO.hPutStrLn IO.stderr) errors
---           exitWith (ExitFailure 1)
-
---         return actions
---       readDirectoryIfOnlyOneWasSpecified :: MTL.StateT (Options, [FilePath]) IO ()
---       readDirectoryIfOnlyOneWasSpecified = do
---         (opts, imgs) <- MTL.get
---         imgs' <- case imgs of
---           [maybeDir] -> MTL.liftIO $ do
---             isExistingDirectory <- doesDirectoryExist maybeDir
---             if isExistingDirectory
---               then do
---                 IO.hPutStrLn IO.stderr ("directory specified: " ++ maybeDir)
---                 imgs' <- map (maybeDir </>) <$> listDirectory maybeDir
---                 return imgs'
---               else return imgs
---           _ -> return imgs
---         MTL.put (opts, imgs')
---       readActionsIntoOpts :: [Options -> IO Options] -> MTL.StateT (Options, [FilePath]) IO ()
---       readActionsIntoOpts actions = do
---         (opts, imgs) <- MTL.get
---         opts' <- MTL.liftIO $ foldl (>>=) (return opts) actions
---         MTL.put (opts', imgs)
---       failIfNoImagesWereSpecified :: MTL.StateT (Options, [FilePath]) IO ()
---       failIfNoImagesWereSpecified = do
---         (opts, imgs) <- MTL.get
---         when (null imgs) $ MTL.liftIO $ do
---           IO.hPutStrLn IO.stderr ("no image specified")
---           exitWith (ExitFailure 1)
---       applyEveryNth :: MTL.StateT (Options, [FilePath]) IO ()
---       applyEveryNth = do
---         (opts, imgs) <- MTL.get
---         case opts of
---           Options {optEveryNth = Just n} -> do
---             let imgs' = everyNth n imgs
---             MTL.put (opts, imgs')
---           _ -> return ()
---       makeImgsPathsAbsoluteAndCheckExistence :: MTL.StateT (Options, [FilePath]) IO ()
---       makeImgsPathsAbsoluteAndCheckExistence = do
---         (opts, imgs) <- MTL.get
---         imgs' <-
---           MTL.liftIO $
---             mapM
---               ( \img -> do
---                   exists <- doesFileExist img
---                   unless exists $ do
---                     IO.hPutStrLn IO.stderr $ "image not found: " ++ img
---                     exitWith (ExitFailure 1)
---                   makeAbsolute img
---               )
---               imgs
---         MTL.put (opts, imgs')
---    in \args -> do
---         actions <- getActionsFromArgs args
---         readDirectoryIfOnlyOneWasSpecified
---         readActionsIntoOpts actions
---         failIfNoImagesWereSpecified
---         applyEveryNth
---         makeImgsPathsAbsoluteAndCheckExistence
-
---         return ()
-
-myPhotoStateAction :: Options -> MTL.StateT (Imgs, Imgs) IO ()
-myPhotoStateAction opts@Options {optVerbose = verbose} = do
-  when (optSortOnCreateDate opts) $ do
-    (imgs, outs) <- MTL.get
-    MTL.liftIO $ IO.hPutStrLn IO.stderr ("INFO: sorting on create date")
-    imgs' <- MTL.liftIO $ sortByCreateDate verbose imgs
-    when (imgs /= imgs') $ do
-      MTL.liftIO $ IO.hPutStrLn IO.stderr ("WARN: sorting on date changed order")
-    MTL.put (imgs', outs)
-
-  wd <- case optWorkdirStrategy opts of
-    CreateNextToImgDir -> do
-      imgs@(img0 : _) <- MTL.gets fst
-      let imgBN = computeStackOutputBN imgs
-      let wd = (takeDirectory img0) <.> imgBN <.> "myphoto"
-      MTL.liftIO $ createDirectoryIfMissing True wd
-      return wd
-    MoveExistingImgsToSubfolder -> do
-      (imgs@(img0 : _), outs) <- MTL.get
-      let wd = takeDirectory img0
-      let imgBN = computeStackOutputBN imgs
-      let indir = wd </> imgBN <.> "raw"
-      imgs' <- MTL.liftIO $ move indir imgs
-      MTL.put (imgs', outs)
-      return wd
-    NextToImgFiles -> do
-      (imgs@(img0 : _), outs) <- MTL.get
-      let wd = takeDirectory img0
-      return wd
-    WorkdirStrategyOverwrite wd -> do
-      MTL.liftIO $ createDirectoryIfMissing True wd
-      absWd <- MTL.liftIO $ makeAbsolute wd
-      return absWd
-  MTL.liftIO $ do
-    setCurrentDirectory wd
-    IO.hPutStrLn IO.stderr ("INFO: work directory: " ++ wd)
-
-  case optBreaking opts of
-    Nothing -> return ()
-    Just gapInSeconds | gapInSeconds < 1 -> return ()
-    Just gapInSeconds -> do
-      case (optEveryNth opts) of
-        Just _ -> do
-          MTL.liftIO $ IO.hPutStrLn IO.stderr ("INFO: ignoring --breaking because --every-nth is specified")
-        _ -> do
-          MTL.liftIO $ IO.hPutStrLn IO.stderr ("INFO: breaking on time gap of " ++ show gapInSeconds ++ " seconds")
-          (imgs, outs) <- MTL.get
-          broken <- MTL.liftIO $ breaking verbose gapInSeconds imgs
-          MTL.put (broken, outs)
-
-  when (optRemoveOutliers opts) $ do
-    MTL.liftIO $ IO.hPutStrLn IO.stderr ("INFO: removing outliers")
-    (imgs, outs) <- MTL.get
-    withoutOutliers <- MTL.liftIO $ rmOutliers wd imgs
-    MTL.put (withoutOutliers, outs)
-
-  outputBN <- do
-    imgs <- MTL.gets fst
-    return (computeStackOutputBN imgs)
-
-  do
-    MTL.liftIO $ IO.hPutStrLn IO.stderr ("INFO: create montage")
-    imgs <- MTL.gets (sampleOfM 25 . fst)
-    montageOut <- MTL.liftIO $ montage 100 (inWorkdir wd (outputBN <.> "all")) imgs
-    MTL.liftIO $ IO.hPutStrLn IO.stderr ("INFO: wrote " ++ montageOut)
-    when (optWorkdirStrategy opts == MoveExistingImgsToSubfolder) $ do
-      MTL.liftIO $ reverseLink (wd </> "..") [montageOut]
-      return ()
-
-  if optFocusStack opts
-    then do
-      MTL.liftIO $ IO.hPutStrLn IO.stderr ("INFO: focus stacking (and aligning) with PetteriAimonen/focus-stack")
-      (imgs, outs) <- MTL.get
-      let additionalParameters = Map.findWithDefault [] "focus-stack" (optParameters opts)
-      (focusStacked, aligned) <- MTL.liftIO $ focusStackImgs verbose additionalParameters imgs
-      MTL.put (aligned, outs ++ [focusStacked])
-      return aligned
-    else do
-      MTL.liftIO $ IO.hPutStrLn IO.stderr ("INFO: just aligning with hugin")
-      (imgs, outs) <- MTL.get
-      aligned <- MTL.liftIO $ align verbose wd imgs
-      MTL.put (aligned, outs)
-      return aligned
-
-  when (optEnfuse opts) $ do
-    MTL.liftIO $ IO.hPutStrLn IO.stderr ("INFO: focus stacking with enfuse")
-    (imgs, outs) <- MTL.get
-    enfuseResult <-
-      MTL.liftIO $
-        enfuseStackImgs
-          ( enfuseDefaultOptions
-              { optOutputBN = Just outputBN,
-                optEnfuseVerbose = verbose
-              }
-          )
-          imgs
-    case enfuseResult of
-      Left err -> MTL.liftIO $ IO.hPutStrLn IO.stderr err
-      Right enfuseOuts -> MTL.put (imgs, outs ++ enfuseOuts)
-
-  -- do
-  --   MTL.liftIO $ IO.hPutStrLn IO.stderr ("INFO: create montage")
-  --   outs <- MTL.gets snd
-  --   montageOut <- MTL.liftIO $ montage 1000 (inWorkdir wd outputBN) outs
-  --   MTL.liftIO $ IO.hPutStrLn IO.stderr ("INFO: wrote " ++ montageOut)
-
-
-  do 
-    (imgs, outs) <- MTL.get
-    outs' <- MTL.liftIO $ mapM makeAbsolute outs
-    MTL.put (imgs, outs')
-    
-  return ()
-
-runMyPhotoStack' :: IO ()
-runMyPhotoStack' = do
-  args <- getArgs
-  (_, (opts, imgs)) <-
-    (MTL.runStateT (getOptionsAndInitialize args) (startOptions, []))
-  IO.hPutStrLn IO.stderr ("DEBUG: " ++ show opts)
-
-  (_, (_, outs)) <- MTL.runStateT (myPhotoStateAction opts) (imgs, [])
-
-  mapM_
-    ( \out -> do
-      IO.hPutStrLn IO.stderr ("RESULT: output: " ++ out)
-    )
-    outs
-
-  IO.hPutStrLn IO.stderr ("Done")
-#endif
-
 runMyPhotoStack'  :: [String] -> IO ()
 runMyPhotoStack' args = do
   let (actions, startImgs, errors) = getOpt RequireOrder options args
@@ -515,6 +311,7 @@ runMyPhotoStack' args = do
           putImgs withoutOutliers
       createMontage :: MyPhotoM ()
       createMontage = do
+        logInfo ("create montage")
         outputBN <- getOutputBN
         imgs <- getImgs
         let imgs' = sampleOfM 25 imgs
@@ -536,21 +333,6 @@ runMyPhotoStack' args = do
         addOut focusStacked
         return aligned
 
-
-          -- if optFocusStack opts
-          --   then do
-          --     MTL.liftIO $ IO.hPutStrLn IO.stderr ("INFO: focus stacking (and aligning) with PetteriAimonen/focus-stack")
-          --     (imgs, outs) <- MTL.get
-          --     let additionalParameters = Map.findWithDefault [] "focus-stack" (optParameters opts)
-          --     (focusStacked, aligned) <- MTL.liftIO $ focusStackImgs verbose additionalParameters imgs
-          --     MTL.put (aligned, outs ++ [focusStacked])
-          --     return aligned
-          --   else do
-          --     MTL.liftIO $ IO.hPutStrLn IO.stderr ("INFO: just aligning with hugin")
-          --     (imgs, outs) <- MTL.get
-          --     aligned <- MTL.liftIO $ align verbose wd imgs
-          --     MTL.put (aligned, outs)
-          --     return aligned
       runHuginAlign :: MyPhotoM [FilePath]
       runHuginAlign = do
         logInfo ("just aligning with hugin")
@@ -564,26 +346,20 @@ runMyPhotoStack' args = do
       runEnfuse aligned = do
         logInfo ("focus stacking with enfuse")
         opts <- getOpts
-        let outputBN = computeStackOutputBN aligned
-        enfuseResult <-
-          MTL.liftIO $
-            enfuseStackImgs
-              ( enfuseDefaultOptions
-                  { optOutputBN = Just outputBN,
-                    optEnfuseVerbose = optVerbose opts
-                  }
-              )
-              aligned
-        case enfuseResult of
-          Left err -> fail err
-          Right enfuseOuts -> addOuts enfuseOuts
-
-          -- -- do
-          -- --   MTL.liftIO $ IO.hPutStrLn IO.stderr ("INFO: create montage")
-          -- --   outs <- MTL.gets snd
-          -- --   montageOut <- MTL.liftIO $ montage 1000 (inWorkdir wd outputBN) outs
-          -- --   MTL.liftIO $ IO.hPutStrLn IO.stderr ("INFO: wrote " ++ montageOut)
-
+        when (optEnfuse opts) $ do
+          let outputBN = computeStackOutputBN aligned
+          enfuseResult <-
+            MTL.liftIO $
+              enfuseStackImgs
+                ( enfuseDefaultOptions
+                    { optOutputBN = Just outputBN,
+                      optEnfuseVerbose = optVerbose opts
+                    }
+                )
+                aligned
+          case enfuseResult of
+            Left err -> fail err
+            Right enfuseOuts -> addOuts enfuseOuts
 
       makeOutsPathsAbsolute :: MyPhotoM ()
       makeOutsPathsAbsolute = do
@@ -599,12 +375,6 @@ runMyPhotoStack' args = do
               )
               outs
 
-
-
-
-
-
-
   let fun = do
         readDirectoryIfOnlyOneWasSpecified
         readActionsIntoOpts actions
@@ -613,15 +383,17 @@ runMyPhotoStack' args = do
         makeImgsPathsAbsoluteAndCheckExistence
         sortOnCreateDate
         wd <- getWdAndMaybeMoveImgs
-        MTL.liftIO $ do
-          setCurrentDirectory wd
-          logInfoIO ("work directory: " ++ wd)
-        outputBN <- getOutputBN
-        applyBreaking
-        applyRemoveOutliers
-        createMontage
-        runFoucsStack
-        makeOutsPathsAbsolute
+        -- redirectLogToLogFile $ do
+        do
+          MTL.liftIO $ do
+            setCurrentDirectory wd
+            logInfoIO ("work directory: " ++ wd)
+          applyBreaking
+          applyRemoveOutliers
+          -- createMontage
+          aligned <- runFoucsStack
+          runEnfuse aligned
+          makeOutsPathsAbsolute
 
     
   (_, endState) <- (MTL.runStateT fun (startMyPhotoState startImgs))
